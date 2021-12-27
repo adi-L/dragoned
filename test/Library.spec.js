ec3dbd23f2dff9eaf0e626715d11bb5a779b3588
adidui <adilev3344@gmail.com>
2021-12-28 01:14:28 +0200

    write tets

--- test/Library.spec.js
+++ test/Library.spec.js
@@ -1,23 +1,39 @@
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 const chai = require('chai');
-const Library = require('../index');
-
+const jsdom = require('jsdom');
+const containerStack = require('../src/containerStack');
+const Dragoned = require('../src/index');
+const getParent = require('../src/scripts/getParent');
 const { expect } = chai;
 
+global.window = new jsdom.JSDOM('<!DOCTYPE html><html><body></body></html>', {
+  url: 'http://localhost'
+}).window;
+global.document = window.document;
+global.HTMLElement = window.HTMLElement;
+
+chai.should();
 chai.use(sinonChai);
 
-let library;
+describe('create new instace of library with null container', function () {
+  it('should return error', function () {
+    expect(new Dragoned.default(null)).to.be.an('error');
+  });
+});
 
-// Replace with actual tests
-describe('Dragoned', () => {
-  beforeEach(() => library = new Library());
 
-  it('should get the library\'s name', () => {
-    const spy = sinon.spy(library, 'name');
-    const name = library.name();
-    expect(name).to.eql('Dragoned');
-    spy.restore();
-    expect(spy).to.have.been.called;
+describe('use getParent function', function () {
+  it('should return the parent node', function () {
+    const container = window.document.createElement('div');
+    const element = window.document.createElement('div');
+    container.appendChild(element);
+    expect(getParent.default(element)).to.be.equal(container);
+  });
+});
+
+describe('empty container stack', function () {
+  it('should return empty array from containerStack', function () {
+    sinon.assert.match(containerStack.default, []);
   });
 });
