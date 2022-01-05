a6c0f29a1ffb95cfbfb5dc3f5f6904eacb6ba9b1
adidui <adilev3344@gmail.com>
2022-01-05 17:39:37 +0200

    create constants

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import detectLeftButton from './scripts/detectLeftButton';
 import getImmediateChild from './scripts/getImmediateChild';
 import renderMirrorImage from './scripts/renderMirrorImage';
 import containerStack from './containerStack';
+import { EVENTS, DIRECTIONS, CLASS_NAMES } from './constants';
 
 export default class Dragoned {
   constructor(container, options = {}) {
@@ -42,7 +43,7 @@ export default class Dragoned {
 
   createGuideLine() {
     this.guideLine = document.createElement('div');
-    this.guideLine.className = '__sortable_draggable-guide-line';
+    this.guideLine.className = CLASS_NAMES.guideLine;
     this.guideLine.style.position = 'absolute';
     this.guideLine.style.borderRadius = `.5rem`;
     this.guideLine.style.backgroundColor = 'rgb(70, 25, 194)';
@@ -61,8 +62,8 @@ export default class Dragoned {
     this.guideLine.remove();
     this.guideLine.style.left = `${-9999}px`;
     this.guideLine.style.top = `${-9999}px`;
-    document.removeEventListener('mousemove', this.onMouseMove);
-    document.removeEventListener('mouseup', this.onMouseUp);
+    document.removeEventListener(EVENTS.MOUSE_MOVE, this.onMouseMove);
+    document.removeEventListener(EVENTS.MOUSE_UP, this.onMouseUp);
     if (this.direction && this.dropEl) {
       const droppableEl = this.options.clone === true ? this.dragEl.cloneNode(true) : this.dragEl;
       this.dropEl.insertAdjacentElement(this.direction, droppableEl);
@@ -87,17 +88,17 @@ export default class Dragoned {
   }
   onMouseMove(event) {
     event.preventDefault();
-    if (event.type === 'mousemove' && !detectLeftButton(event)) {
+    if (event.type === EVENTS.MOUSE_MOVE && !detectLeftButton(event)) {
       this.guideLine.remove();
-      document.removeEventListener('mousemove', this.onMouseMove);
+      document.removeEventListener(EVENTS.MOUSE_MOVE, this.onMouseMove);
     }
-    const clientY = event.type === 'touchmove' ? event.touches[0].clientY : event.clientY;
-    const clientX = event.type === 'touchmove' ? event.touches[0].clientX : event.clientX;
-    const pageY = event.type === 'touchmove' ? event.touches[0].pageY : event.pageY;
+    const clientY = event.type === EVENTS.TOUCH_MOVE ? event.touches[0].clientY : event.clientY;
+    const clientX = event.type === EVENTS.TOUCH_MOVE ? event.touches[0].clientX : event.clientX;
+    const pageY = event.type === EVENTS.TOUCH_MOVE ? event.touches[0].pageY : event.pageY;
     if (clientY < this.moveY) {
-      this.mouseDirection = 'top';
+      this.mouseDirection = DIRECTIONS.TOP;
     } else if (clientY > this.moveY) {
-      this.mouseDirection = 'bottom';
+      this.mouseDirection = DIRECTIONS.BOTTOM;
     }
     this.moveY = clientY;
     const scrollUp = () => window.scrollTo({
@@ -109,12 +110,12 @@ export default class Dragoned {
       left: 0
     });
     const scroller = () => {
-      if (this.mirror && this.moveY < 100 && this.mouseDirection === 'top') {
+      if (this.mirror && this.moveY < 100 && this.mouseDirection === DIRECTIONS.TOP) {
         scrollUp();
         setTimeout(() => {
           scroller();
         }, 100);
-      } else if (this.mirror && window.innerHeight - this.moveY < 100 && this.mouseDirection === 'bottom') {
+      } else if (this.mirror && window.innerHeight - this.moveY < 100 && this.mouseDirection === DIRECTIONS.BOTTOM) {
         scrollBottom();
         setTimeout(() => {
           scroller();
@@ -164,7 +165,7 @@ export default class Dragoned {
       this.guideLine.style.height = '4px';
       // is mouse is on the top of the element
       if (rect.bottom > this.moveY && rect.bottom - rect.height / 2 < this.moveY) {
-        this.direction = 'afterend';
+        this.direction = DIRECTIONS.AFTEREND;
         this.dropEl = dropEl;
         this.dragEl = this.dragEl;
         this.guideLine.style.top = `${pageY - pageY + window.pageYOffset
@@ -172,7 +173,7 @@ export default class Dragoned {
         this.guideLine.style.left = `${rect.left}px`;
       } else if (rect.top < this.moveY && rect.top + rect.height / 2 > this.moveY) {
         this.dropEl = dropEl;
-        this.direction = 'beforebegin';
+        this.direction = DIRECTIONS.BEFOREBEGIN;
         this.guideLine.style.top = `${pageY - pageY + window.pageYOffset
           + rect.top}px`;
         this.guideLine.style.left = `${rect.left}px`;
@@ -183,12 +184,12 @@ export default class Dragoned {
   }
 
   dragStart(event) {
-    if (event.type === 'mousedown' && !detectLeftButton(event)) {
+    if (event.type === EVENTS.MOUSE_DOWN && !detectLeftButton(event)) {
       return;
     }
     document.body.appendChild(this.guideLine);
-    const clientY = event.type === 'touchstart' ? event.touches[0].clientY : event.clientY;
-    const clientX = event.type === 'touchstart' ? event.touches[0].clientX : event.clientX;
+    const clientY = event.type === EVENTS.TOUCH_START ? event.touches[0].clientY : event.clientY;
+    const clientX = event.type === EVENTS.TOUCH_START ? event.touches[0].clientX : event.clientX;
     const target = event.target;
     let draggableEl;
     let handleEl;
@@ -215,25 +216,25 @@ export default class Dragoned {
     this.dragEl = dragEl;
     this.dragEl.Sortable__container__ = this.container;
     this.oldIndex = Array.prototype.indexOf.call(this.container.children, dragEl);
-    document.addEventListener('mousemove', this.onMouseMove);
-    document.addEventListener('touchmove', this.onMouseMove, { passive: false });
-    document.addEventListener('mouseup', this.onMouseUp);
-    document.addEventListener('touchend', this.onMouseUp);
+    document.addEventListener(EVENTS.MOUSE_MOVE, this.onMouseMove);
+    document.addEventListener(EVENTS.TOUCH_MOVE, this.onMouseMove, { passive: false });
+    document.addEventListener(EVENTS.MOUSE_UP, this.onMouseUp);
+    document.addEventListener(EVENTS.TOUCH_END, this.onMouseUp);
 
   }
 
   bindDrag(container) {
     container.style.userSelect = 'none';
-    container.addEventListener('mousedown', this.dragStart);
-    container.addEventListener('touchstart', this.dragStart);
+    container.addEventListener(EVENTS.MOUSE_DOWN, this.dragStart);
+    container.addEventListener(EVENTS.TOUCH_START, this.dragStart);
   }
   destroy() {
     const index = containerStack.findIndex(c => c === this);
     if (index !== -1) {
       containerStack.splice(index, 1);
     }
-    this.container.removeEventListener('mousedown', this.dragStart);
-    this.container.removeEventListener('touchstart', this.dragStart);
+    this.container.removeEventListener(EVENTS.MOUSE_DOWN, this.dragStart);
+    this.container.removeEventListener(EVENTS.TOUCH_START, this.dragStart);
     if (this.mirror) {
       this.mirror.remove();
     }
